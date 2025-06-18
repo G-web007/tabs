f05dd599e957aad92ff15f32a9520d4dcb4a4a5c
Vincent Ygbuhay <ygbuhay.vincent@yahoo.com>
2025-06-18 10:54:21 +0800

    add javaScript functionality

--- main.js
+++ main.js
@@ -1,1 +1,28 @@
-console.log('Hello World!');
+const elements = document.querySelectorAll('.tablinks');
+for (let i = 0; i < elements.length; i++) {
+    elements[i].addEventListener('click', function(event) {
+        const targetId = this.getAttribute('data-target');
+        openCity(event, targetId);
+    });
+}
+
+function openCity(evt, targetId) {
+    let tabcontent = document.getElementsByClassName('tabcontent');
+    for (let i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+
+    let tablinks = document.getElementsByClassName('tablinks');
+    for (let i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", ""); 
+    }
+
+    const targetElement = document.getElementById(targetId);
+    if (targetElement) {
+        targetElement.style.display = "block";
+        evt.currentTarget.className += " active";
+    } else {
+        console.error("No element found with ID:", targetId);
+    }
+}
+
